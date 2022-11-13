5715aedd52bcdf2698c22b9391888ba64a4c90da
alufers <alufers@wp.pl>
2022-11-13 18:01:03 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-unstable }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        darwinPackages = if pkgs.stdenv.isDarwin then [
+          pkgs.darwin.apple_sdk.frameworks.CoreServices
+          pkgs.darwin.apple_sdk.frameworks.ApplicationServices
+          pkgs.darwin.apple_sdk.frameworks.Security
+        ] else
+          [ pkgs-unstable.nrf-command-line-tools
+        ];
+        nrf5-sdk = (pkgs.callPackage ./nix/nrf5.nix { });
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                cmake
+                ninja
+                nrf5-sdk
+                gcc-arm-embedded
+
+              ] ++ darwinPackages;
+            shellHook = ''
+              export SDK_ROOT=${nrf5-sdk.outPath}/share/nRF5_SDK
+            '';
+          };
+        };
+      });
+}
